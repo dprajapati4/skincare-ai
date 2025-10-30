6016fd026b390901d7b5acc109832adc5c1459ea
Deep <dprajap2@gmail.com>
2025-10-30 12:24:58 -0400

    update heading

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,20 @@
 "use client";
 
 import Form from "../components/Form";
-import Skeleton from "@/components/Skeleton";
 
 export default function Home() {
   return (
     <main className=" min-h-screen min-w-md mx-auto flex flex-col items-center justify-center text-charcoal">
-      <h1 className="text-mauve text-4xl text-center font-semibold mb-6 p-4">
-        Discover Your Perfect Skincare Routine
+      <h1
+        className="text-center text-5xl font-extrabold m-4 bg-gradient-to-r from-[var(--color-mauve)] via-[var(--color-blush)] to-[var(--color-gold)] bg-clip-text text-transparent tracking-tight drop-shadow-md animate-gradient text-shadow-sm
+"
+      >
+        Skincare AI
       </h1>
+
+      <h2 className="text-mauve text-2xl text-center font-semibold mb-6 p-4">
+        Discover Your Perfect Skincare Routine
+      </h2>
       <p className="text-lg text-ink mb-12 text-center">
         Find personalized skincare products that suit your skin&apos;s unique
         needs. Let&apos;s get started!
